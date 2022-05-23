b570a244395f0b2a4804210b48e732a163e6744c
titu-coder <marinescu.titu1@gmail.com>
2022-05-23 15:35:29 +0300

    "StrictMode" added to entire project.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import './index.css';
 
@@ -9,4 +7,8 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>);
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
