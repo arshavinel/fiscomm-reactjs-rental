ef49ed3af910c61df43da11b0ca2dc89c3bab58b
titu-coder <marinescu.titu1@gmail.com>
2022-05-23 21:05:21 +0300

    Import Bootstrap module in index.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,3 +11,5 @@ root.render(
         <App/>
     </React.StrictMode>
 );
+
+
