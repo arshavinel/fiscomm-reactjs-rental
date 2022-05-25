31f9d24da581f5388dfbb70540ecc112047fc134
titu-coder <marinescu.titu1@gmail.com>
2022-05-25 09:40:54 +0300

    Chat button (Without chat modal)

--- src/Components/ChatButton.jsx
+++ src/Components/ChatButton.jsx
@@ -3,14 +3,13 @@ import './ChatButton.scss';
 
 const ChatButton = ()=>{
     return(
-
-        <div className="btnChat">
-            <div>
-            <h2></h2>
+        <div className="chat-button">
+            <div className="cardImage user-logo">
+               <img className="" src={require('../img/user-logo.png')} alt='booking' />
             </div>
+            <h5 className="button-chat-text">Let's chat!</h5>  
         </div>
-       
     )
 }
 
-export default ChatButton
+export default ChatButton;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,12 +2,15 @@ import Hero from "../Components/Hero";
 import './Homepage.css';
 import React from "react";
 import Footer from "../Components/Footer";
+import ChatButton from "../Components/ChatButton"
 
 export default function Homepage() {
   return (
-    <div className="hero-parent">
+    <div className="hero-parent">  
       <Hero />
+      <ChatButton/>
       <Footer/>
     </div>
+
   )
 }
