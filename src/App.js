f4a4602ea3e0cb3a74e023aefdd6030edd0d7b4e
Alex.Ghighe <ghighe88@gmail.com>
2022-06-07 22:34:29 +0300

    basic modal functionality added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.scss';
 
 
 function App() {
+
   return (
 
     <div className='App'>
--- src/Components/ChatModal.jsx
+++ src/Components/ChatModal.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import "./ChatModal.scss";
+import {useState} from 'react'
 
 function ChatModal({ closeModal }){
+
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+
+    const handleSendMessage = () => {
+        if(message){
+            setMessages([...messages,message]);
+            setMessage('');
+        }
+    }
+
+    console.log("Messages", messages);
+
+
     return(
 
             <div className="chatModalContainer">
-
-                   <button className="button-close" onClick={()=>{closeModal(false)}}>Close!!</button>
-
+                <div className="chat-header-text">
+                    <h5>rio-apartment-rental</h5>
+                    <p style={{marginLeft:'10px'}}>We’ll reply as soon as we can</p>
+                </div>
+                <button className="button-close" onClick={()=>{closeModal(false)}}>X</button>
+                <div id="chat-content" name="chat-content">
+                    {messages.map(message =>
+                    <li key={message.length}>{message}</li>
+                    )}
+                </div>
+                <div className="buttonIn">
+                    <input type="text"
+                    value={message}
+                    id="text-area"
+                    placeholder="Type a message..."
+                    onChange={e => setMessage(e.target.value)} />
+                    <button
+                    className="sendMessage"
+                    onClick={handleSendMessage}>Send</button>
+                </div>
             </div>
 
     )
