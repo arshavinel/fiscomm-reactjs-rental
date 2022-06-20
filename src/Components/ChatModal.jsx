e44b8a4857da1ea6d92580e3574e9336fbd847bd
Alex.Ghighe <ghighe88@gmail.com>
2022-06-20 14:21:29 +0300

    adding send-icon , chat functionality was implemented

--- src/Components/ChatModal.jsx
+++ src/Components/ChatModal.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import "./ChatModal.scss";
-import {useState} from 'react'
+import {useState, useRef} from 'react'
+import { useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function ChatModal({ closeModal }){
 
+    const messagesEndRef = useRef(null);
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
+    const [displayIcon, setDisplayIcon] = useState(false);
 
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({behaviour: "smooth"})
+    }
+
+    useEffect(scrollToBottom, [messages]);
 
     const handleSendMessage = () => {
         if(message){
             setMessages([...messages,message]);
             setMessage('');
+           setDisplayIcon(false);
         }
     }
 
-    console.log("Messages", messages);
+    const onPressEnter = e => {
+        e.preventDefault();
+         handleSendMessage();
+    }
+
+    const onTypingText = (e) => {
+        setMessage(e.target.value);
+        if(e.target.value !== ''){
+            setDisplayIcon(true);
+        }else{
+            setDisplayIcon(false);
+        }
+    }
+
+    // console.log("Messages", messages);
 
 
     return(
-
             <div className="chatModalContainer">
                 <div className="chat-header-text">
                     <h5>rio-apartment-rental</h5>
@@ -28,21 +52,26 @@ function ChatModal({ closeModal }){
                 <button className="button-close" onClick={()=>{closeModal(false)}}>X</button>
                 <div id="chat-content" name="chat-content">
                     {messages.map(message =>
-                    <li key={message.length}>{message}</li>
+                    <div className="textMessage" key={uuidv4()}>
+                    <p>{message}</p>
+                    </div>
                     )}
+                    <div ref={messagesEndRef} />
                 </div>
-                <div className="buttonIn">
+                <form id="form" onSubmit={onPressEnter}>
                     <input type="text"
                     value={message}
                     id="text-area"
+                    autoComplete="off"
                     placeholder="Type a message..."
-                    onChange={e => setMessage(e.target.value)} />
-                    <button
+                    onChange={onTypingText} />
+                   {displayIcon && <img src={require('../img/send-icon.png')}
+                    alt="send-icon"
                     className="sendMessage"
-                    onClick={handleSendMessage}>Send</button>
+                    onClick={handleSendMessage}
+                    />}
+                </form>
                 </div>
-            </div>
-
     )
 }
 
